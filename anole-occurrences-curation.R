fbbe6c1cd06a962bdcb574dd3f737d122fb97ada
Tanner C. Myers <tannercmyers@gmail.com>
2021-10-04 23:22:33 -0500

    Adding code to clean occurrence data.
    Adding code to remove NAs and duplicates and exclude occurrences from outside the range of Anolis cybotes.

--- anole-occurrences-curation.R
+++ anole-occurrences-curation.R
@@ -35,7 +35,7 @@ gbif_occs <- cyb$gbif$data$Anolis_cybotes
 head(gbif_occs)
 
 ## write gbif_occs to file to create a derived dataset – this gives us a citable DOI for the GBIF data we're using
-write_csv(gbif_occs, "data/GBIF/Anolis_cybotes_GBIF_download.csv")
+write_csv(gbif_occs, "GBIF/Anolis_cybotes_GBIF_download.csv")
 
 ## saving initial data with various columns from GBIF
 colnames(gbif_occs)
@@ -44,3 +44,24 @@ columns <- c("name", "longitude", "latitude", "issues", "scientificName",
              "countryCode", "locality", "elevation")
 
 occs <- gbif_occs[, columns]
+
+# Plot occurrence points
+hispaniola <- map_data("world")%>%subset(region == c("Haiti", "Dominican Republic"))
+ggplot() +
+  geom_polygon(data=hispaniola, aes(x=long, y=lat, group=group), color='lightgrey', fill=NA) +
+  geom_point(data=occs, aes(x=longitude, y=latitude), shape=1, size=2, alpha=0.85, color="green") + 
+  coord_fixed(xlim=c(-75,-67.5), ylim=c(17,20), ratio=1.3) + theme_nothing()
+
+#---------------------------------------------------------------------------------
+
+#--------------------- Data cleaning ---------------------------------------------
+## subset columns of interest
+occ <- occs[, c("scientificName", "longitude", "latitude")]
+occ <- na.omit(occ) # exclude NAs here after reducing number of fields in dataframe
+
+## excluding 0, 0 coordinates 
+occ <- occ[occ$longitude != 0 & occ$latitude != 0, ]
+
+## excluding duplicates
+occ <- occ[!duplicated(paste(occ$longitude, occ$latitude)), ]
+
